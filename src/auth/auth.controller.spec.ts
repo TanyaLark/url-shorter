e313e91ee3988ef70f3dcae7b2d27fa8c557e250
Tetiana Niehrieieva <tetiana.niehrieieva@itomy.ch>
2024-08-05 12:10:39 +0300

    Refactored unit tests for Auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
-import { SignUpResDto } from './dto/sign-up-res.dto';
+import { RegisterResponseDto } from './dto/sign-up-res.dto';
 import { HttpStatus } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { User, UserRole } from '../users/user.entity';
+import { SerializedUser } from './interceptors/serialized-registered-user';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -17,7 +18,7 @@ describe('AuthController', () => {
       imports: [
         JwtModule.register({
           global: true,
-          secret: jwtConstants.secret,
+          secret: process.env.JWT_SECRET,
           signOptions: { expiresIn: '60s' },
         }),
       ],
@@ -49,24 +50,44 @@ describe('AuthController', () => {
         email: 'taniaben@gmail.com',
         password: 'taniaSuperPass@1',
       };
-      const signUpResponse: SignUpResDto = {
+      const createdAtMock = new Date();
+      const userMock: User = {
+        id: '1',
+        firstName: createUserDto.firstName,
+        lastName: createUserDto.lastName,
+        email: createUserDto.email,
+        emailConfirmed: true,
+        emailConfirmToken: 'token',
+        passwordHash: 'passwordHash',
+        salt: 'salt',
+        role: UserRole.User,
+        isActive: true,
+        createdAt: createdAtMock,
+        updatedAt: null,
+        urls: [],
+        updateTimestamp: jest.fn(),
+      };
+      const signUpResponse: RegisterResponseDto = {
         status: HttpStatus.CREATED,
         description: 'User registered',
+        user: new SerializedUser(userMock),
       };
 
       (authService.signUp as jest.Mock).mockResolvedValue(signUpResponse);
-
+      jest.spyOn(authService, 'signUp').mockResolvedValue(userMock);
       const result = await controller.signUp(createUserDto);
-      expect(result).toEqual(signUpResponse);
       expect(authService.signUp).toHaveBeenCalledWith(createUserDto);
+      expect(result.status).toEqual(signUpResponse.status);
+      expect(result.description).toEqual(signUpResponse.description);
+      expect(result.user).toEqual(signUpResponse.user);
     });
   });
 
   describe('signIn', () => {
     it('should login a user successfully', async () => {
       const loginDto: LoginDto = {
-        username: 'Tania Benjamin',
-        password: 'password',
+        email: 'taniaben@gmail.com',
+        password: 'taniaSuperPass@1',
       };
       const tokenResponse = { access_token: 'testToken' };
 
@@ -75,20 +96,9 @@ describe('AuthController', () => {
       const result = await controller.signIn(loginDto);
       expect(result).toEqual(tokenResponse);
       expect(authService.signIn).toHaveBeenCalledWith(
-        loginDto.username,
+        loginDto.email,
         loginDto.password,
       );
-    });
-  });
-
-  describe('getProfile', () => {
-    it('should return user profile', async () => {
-      const req = {
-        user: { id: 1, username: 'Tania Benjamin' },
-      };
-
-      const result = controller.getProfile(req);
-      expect(result).toEqual(req.user);
     });
   });
 });
