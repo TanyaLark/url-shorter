9c3a7721cc755c2feb91f85b45b490b0d3024759
Tetiana Niehrieieva <tetiana.niehrieieva@itomy.ch>
2024-08-15 11:40:32 +0300

    Added e2e PATCH /team/update/id/:teamId

--- test/team.e2e-spec.ts
+++ test/team.e2e-spec.ts
@@ -10,6 +10,8 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreateUserDto } from '../src/users/dtos/create-user.dto';
 import { CreateTeamDto } from '../src/team/dtos/create-team.dto';
 import { SerializedTeam } from '../src/team/interceptors/serialized-team';
+import { UpdateTeamDto } from '../src/team/dtos/update-team.dto';
+import { SerializedUpdatedTeam } from '../src/team/interceptors/serialized-updated-team';
 
 describe('TeamController (e2e)', () => {
   let app: INestApplication;
@@ -99,6 +101,67 @@ describe('TeamController (e2e)', () => {
         .post('/team/create')
         .send(teamDto)
         .expect(401);
+    });
+  });
+
+  describe('PATCH /team/update/id/:teamId', () => {
+    let teamId: string;
+    const createDto: CreateTeamDto = { name: 'TeamE2EUpdate' };
+    const updateDto: UpdateTeamDto = { name: 'TeamE2EUpdated' };
+
+    beforeAll(async () => {
+      const responseTeam = await request(app.getHttpServer())
+        .post('/team/create')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .send(createDto);
+
+      teamId = JSON.parse(responseTeam.text).id;
+    });
+
+    it('should update the team information', async () => {
+      const responseTeam = await request(app.getHttpServer())
+        .patch(`/team/update/id/${teamId}`)
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .send(updateDto);
+
+      const responseBody: SerializedUpdatedTeam = JSON.parse(responseTeam.text);
+      expect(responseTeam.status).toBe(200);
+      expect(responseBody.name).toBe(updateDto.name);
+      expect(responseBody.icon).toBeDefined();
+      expect(responseBody.createdAt).toBeDefined();
+      expect(responseBody.updatedAt).toBeDefined();
+      expect(responseBody.id).toBeDefined();
+    });
+
+    it('should return 400 if the teamId invalid', async () => {
+      return request(app.getHttpServer())
+        .patch(`/team/update/id/invalidId`)
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .send({ name: 'TeamE2EUpdated' })
+        .expect(400);
+    });
+
+    it('should return 401 if the JWT token is missing', async () => {
+      return request(app.getHttpServer())
+        .patch(`/team/update/id/${teamId}`)
+        .send({ name: 'TeamE2EUpdated' })
+        .expect(401);
+    });
+
+    it('should return 404 if the team does not exist', async () => {
+      return request(app.getHttpServer())
+        .patch(`/team/update/id/123e4567-e89b-12d3-a456-426614174000`)
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .send({ name: 'TeamE2EUpdated' })
+        .expect(404);
+    });
+
+    afterAll(async () => {
+      await teamRepository.remove(
+        await teamRepository.find({
+          where: { name: updateDto.name },
+        }),
+      );
     });
   });
 });
