47a0b168d834818a6a27e1b2a5bf64903c489d75
Tetiana Niehrieieva <tetiana.niehrieieva@itomy.ch>
2024-08-20 12:03:13 +0300

    Added Team repository in Users module. Reworked create user method in uses service. Removed comments.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
+import { TeamRepository } from '../team/team.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, UsersRepository],
+  providers: [UsersService, UsersRepository, TeamRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,15 @@ import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './user.entity';
+import { TeamRepository } from '../team/team.repository';
+import { CreateTeamDto } from '../team/dtos/create-team.dto';
 @Injectable()
 export class UsersService {
   private logger = new Logger(UsersService.name);
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly teamRepository: TeamRepository,
+  ) {}
 
   async create(user: CreateUserDto): Promise<User> {
     const { email } = user;
@@ -17,7 +22,10 @@ export class UsersService {
     }
 
     try {
-      return this.usersRepository.store(user);
+      const createTeamDto: CreateTeamDto = { name: 'Home' };
+      const createdUser = await this.usersRepository.store(user);
+      await this.teamRepository.createTeam(createTeamDto, createdUser);
+      return createdUser;
     } catch (error) {
       this.logger.log(`UsersService:create: ${JSON.stringify(error.message)}`);
       throw new Error(error.message);
@@ -36,52 +44,4 @@ export class UsersService {
       throw new Error(error.message);
     }
   }
-
-  // async findById(id: string): Promise<SerializedUser> {
-  //   try {
-  //     const user = await this.usersRepository.findById(id);
-  //     if (!user) {
-  //       throw new Error('User not found.');
-  //     }
-  //     return user;
-  //   } catch (error) {
-  //     this.logger.log(
-  //       `UsersService:findById: ${JSON.stringify(error.message)}`,
-  //     );
-  //     throw new Error(error.message);
-  //   }
-  // }
-
-  // public async findAll(): Promise<User[]> {
-  //   try {
-  //     const users = await this.usersRepository.findAll();
-  //     if (users?.length === 0) {
-  //       throw new Error('No record found.');
-  //     }
-  //     return users;
-  //   } catch (error) {
-  //     this.logger.log(
-  //       `UsersService:findAll : ${JSON.stringify(error.message)}`,
-  //     );
-  //   }
-  // }
-
-  // async update(id: number, user: User): Promise<User> {
-  //   try {
-  //     await this.findById(id);
-  //     return await this.userRepository.updateOne(id, user);
-  //   } catch (error) {
-  //     this.logger.log(`UsersService:update: ${JSON.stringify(error.message)}`);
-  //     throw new Error(error.message);
-  //   }
-  // }
-
-  // async delete(id: number) {
-  //   try {
-  //     return await this.userRepository.destroy(id);
-  //   } catch (error) {
-  //     this.logger.log(`UsersService:delete: ${JSON.stringify(error.message)}`);
-  //     throw new Error(error.message);
-  //   }
-  // }
 }
